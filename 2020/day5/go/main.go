4033a345fa3196b58f96fb6d19c9212efba963e0
Denes Stolte <contact@stolte.me>
2021-02-03 11:59:25 +0100

    Day 5 Part 1

---
+++ 2020/day5/go/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	bp := LoadBoardingPasses("input.txt")
+
+	sort.Slice(*bp, func(i, j int) bool {
+		return (*bp)[i].SeatID < (*bp)[j].SeatID
+	})
+
+	fmt.Printf("The lost seat id is %d.\n", (*bp)[len(*bp)-1].SeatID)
+
+	var ids []int64
+
+	for _, p := range *bp {
+		ids = append(ids, p.SeatID)
+	}
+
+	fmt.Println(ids)
+}
+
+type BoardingPass struct {
+	bsp    string
+	SeatID int64
+}
+
+func LoadBoardingPasses(path string) *[]BoardingPass {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var bpass []BoardingPass
+
+	for scanner.Scan() {
+		var p BoardingPass
+
+		p.bsp = scanner.Text()
+
+		replacer := strings.NewReplacer("F", "0", "B", "1", "L", "0", "R", "1")
+		binary := replacer.Replace(p.bsp)
+
+		row, _ := strconv.ParseInt(binary[0:7], 2, 32)
+		column, _ := strconv.ParseInt(binary[7:10], 2, 32)
+
+		p.SeatID = row*8 + column
+
+		bpass = append(bpass, p)
+	}
+
+	return &bpass
+}
