49f40538eacc990c5da3323c06922e5ab64a3c60
Denes Stolte <contact@stolte.me>
2021-06-10 21:21:34 +0200

    Day 9

--- 2020/day9/go/main.go
+++ 2020/day9/go/main.go
@@ -9,8 +9,10 @@ import (
 
 func main() {
 	sequence := LoadSequence("input.txt")
-	fmt.Printf("Part One: %d\n", PartOne(sequence))
-
+	partOne := PartOne(sequence)
+	fmt.Printf("Part One: %d\n", partOne)
+	partTwo := PartTwo(sequence, partOne)
+	fmt.Printf("Part Two: %d\n", partTwo)
 }
 
 func LoadSequence(input string) *[]int {
@@ -32,27 +34,58 @@ func LoadSequence(input string) *[]int {
 	return &sequence
 }
 
-func isValidNumber(sequence []int, number int) bool {
-	for _, i := range sequence {
-		current := i - number
-		if current < 0 {
-			current = number - i
-		}
-		for _, j := range sequence {
-			if current == j {
-				return true
-			}
+func isValid(sequence []int, number int) bool {
+	var set map[int]struct{}
+	set = make(map[int]struct{})
+
+	for _, num := range sequence {
+		set[number-num] = struct{}{}
+	}
+	for _, num := range sequence {
+		if _, ok := set[num]; ok {
+			return true
 		}
 	}
 	return false
-
 }
 
 func PartOne(sequence *[]int) int {
 	for i := 25; i < len(*sequence); i++ {
-		if !isValidNumber((*sequence)[i-25:i], (*sequence)[i]) {
+		if !isValid((*sequence)[i-25:i], (*sequence)[i]) {
 			return (*sequence)[i]
 		}
 	}
 	return -1
+}
+
+func findContiguous(sequence *[]int, target int) (int, int) {
+Outer:
+	for start := 0; start < len(*sequence); start++ {
+		var sum, big int
+		var small = target
+
+		for i := start; i < len(*sequence); i++ {
+			val := (*sequence)[i]
+			sum += val
+
+			if val > big {
+				big = val
+			}
+			if val < small {
+				small = val
+			}
+
+			if sum == target {
+				return small, big
+			} else if sum > target {
+				continue Outer
+			}
+		}
+	}
+	return -1, -1
+}
+
+func PartTwo(sequence *[]int, target int) int {
+	small, big := findContiguous(sequence, target)
+	return small + big
 }
