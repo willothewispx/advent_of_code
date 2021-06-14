9c9570ecc0d51a9ed54dbbc52a3f3beba9341928
Denes Stolte <contact@stolte.me>
2021-06-14 13:31:53 +0200

    Day 10

--- 2020/day10/go/main.go
+++ 2020/day10/go/main.go
@@ -8,18 +8,48 @@ import (
 )
 
 func main() {
-	fmt.Printf("Problem One: %d\n", ProblemOne("input.txt"))
-
+	one, adapters := ProblemOne("input.txt")
+	fmt.Printf("Problem One: %d\n", one)
+	two := ProblemTwo(adapters)
+	fmt.Printf("Problem Two: %d\n", two)
 }
 
-func ProblemTwo(path string) int {
-	// Count the number of unnecessary adapters
-	return -1
+// Possible combinations (difference of two does not occur):
+// 3 - 3 (e.g. 4, 7, 10) => only 1 possible configuration
+// 3 - 1 - 3 (e.g. 4, 7, 8, 11) => only 1 possible configuration
+// 3 - 1 - 1 - 3 (e.g. 4, 7, 8, 9, 12) => 2 possible configurations (drop the middle number or don't)
+// 3 - 1 - 1 - 1 - 3 (e.g. 4, 7, 8, 9, 10, 13) => 4 possible configurations (2 each for 8 and 9)
+// 3 - 1 - 1 - 1 - 1 - 3 (e.g. 4, 7, 8, 9, 10, 11, 14) => 7 possible configurations (at least one of 8, 9, 10 must be present, so 2^3 - 1 since dropping all 3 not an option)
+// There are no other configurations
+func ProblemTwo(adapters *[]int) int {
+	mult := 1
+	var counter int
+	var curr int
+
+	for _, adapter := range *adapters {
+		if adapter-curr == 1 {
+			counter++
+			curr += 1
+		} else if adapter-curr == 3 {
+			switch counter {
+			case 2:
+				mult *= 2
+			case 3:
+				mult *= 4
+			case 4:
+				mult *= 7
+			}
+			counter = 0
+			curr += 3
+		}
+	}
+	return mult
 }
 
-func ProblemOne(path string) int {
+func ProblemOne(path string) (int, *[]int) {
 	adapters := LoadAdapters(path)
 	adapterSet := makeSet(adapters)
+	var sorted []int
 	var curr int
 	var oneJolt int
 	var threeJolt int
@@ -28,6 +58,7 @@ func ProblemOne(path string) int {
 		for j := 1; j < 4; j++ {
 			if _, ok := (*adapterSet)[curr+j]; ok {
 				curr += j
+				sorted = append(sorted, curr)
 				if j == 1 {
 					oneJolt++
 				} else {
@@ -37,7 +68,7 @@ func ProblemOne(path string) int {
 			}
 		}
 	}
-	return oneJolt * threeJolt
+	return oneJolt * threeJolt, &sorted
 }
 
 func makeSet(adapters *[]int) *map[int]struct{} {
@@ -70,5 +101,4 @@ func LoadAdapters(path string) *[]int {
 	adapters = append(adapters, max+3)
 
 	return &adapters
-
 }
